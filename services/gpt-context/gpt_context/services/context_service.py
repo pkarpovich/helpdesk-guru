533378bfd90d2db69439af3d8986d8243ca6ecf9
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-07-04 23:35:40 +0200

    refactor(context_service.py): extract file loader selection logic to a separate method for better code organization and readability feat(context_service.py): add type hinting to the new method to improve code clarity and maintainability

--- services/gpt-context/gpt_context/services/context_service.py
+++ services/gpt-context/gpt_context/services/context_service.py
@@ -1,6 +1,7 @@
 import os
 
 from langchain.document_loaders import TextLoader, PDFMinerLoader, CSVLoader
+from langchain.document_loaders.base import BaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from gpt_context.adapters.vector_store_adapter import VectorStoreAdapter
@@ -11,33 +12,38 @@ class ContextService:
         self.store = store
 
     def init(self) -> None:
-        for root, dirs, files in os.walk("source_documents"):
-            loader = None
-
+        for root, _, files in os.walk("source_documents"):
             if not files:
                 print(f"The directory '{root}' is empty.")
                 return
 
             for file in files:
-                ext = os.path.splitext(file)[-1].lower()
+                loader = self._get_loader_for_file(root, file)
 
-                match ext:
-                    case ".txt":
-                        loader = TextLoader(os.path.join(root, file), encoding="utf8")
-                    case ".pdf":
-                        loader = PDFMinerLoader(os.path.join(root, file))
-                    case ".csv":
-                        loader = CSVLoader(os.path.join(root, file))
+                if loader is None:
+                    print("No loader found")
+                    return
 
-            if loader is None:
-                print("No loader found")
-                return None
+                documents = loader.load()
+                text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+                texts = text_splitter.split_documents(documents)
 
-            documents = loader.load()
-            text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-            texts = text_splitter.split_documents(documents)
-
-            self.store.from_documents(texts)
+                self.store.from_documents(texts)
 
     def clear_index(self) -> None:
         self.store.truncate()
+
+    @staticmethod
+    def _get_loader_for_file(root: str, file: str) -> BaseLoader:
+        ext = os.path.splitext(file)[-1].lower()
+        loader = None
+
+        match ext:
+            case ".txt":
+                loader = TextLoader(os.path.join(root, file), encoding="utf8")
+            case ".pdf":
+                loader = PDFMinerLoader(os.path.join(root, file))
+            case ".csv":
+                loader = CSVLoader(os.path.join(root, file))
+
+        return loader
