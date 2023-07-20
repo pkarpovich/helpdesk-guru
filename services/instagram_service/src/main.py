e8e442f5ff9932dab1519e1d7b2394b4d6da9821
MaximPyanin <132012194+MaximPyanin@users.noreply.github.com>
2023-07-20 10:38:28 +0300

    Update main.py

--- services/instagram_service/src/main.py
+++ services/instagram_service/src/main.py
@@ -1,17 +1,15 @@
 from instagrapi import Client
 class InstagramService:
     def __init__(self, username: str, password: str, proxy: str = None) -> None:
-        if proxy:
-            self.client = Client(proxy=proxy)
-        else:
-            self.client = Client()
-        self.client.login(username, password)
-
+       self.client = Client(proxy=proxy)
+        try:
+            self.client.login(username, password)
+        except Exception as e:
+            print(f"Error while setting up Instagram client: {e}")
     def send_direct_message(self, recipient_username: str, message: str) -> None:
         recipient_id = self.client.user_id_from_username(recipient_username)
         recipient_id = int(recipient_id)
         self.client.direct_send(message, [recipient_id])
-
     def read_direct_messages(self) -> None:
         threads = self.client.direct_threads()
         for thread in threads:
