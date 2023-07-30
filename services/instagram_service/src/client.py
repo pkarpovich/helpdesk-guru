23250e90da48ac33f79ddaa162f17e59624d2744
MaximPyanin <132012194+MaximPyanin@users.noreply.github.com>
2023-07-30 22:14:12 +0300

    Update client.py

--- services/instagram_service/src/client.py
+++ services/instagram_service/src/client.py
@@ -2,15 +2,19 @@ import grpc
 import gpt_pb2 as gpt
 import gpt_pb2_grpc as gpt_grpc
 class client_grpc:
-   def __int__(self,target,contextName,conversationId):
-       self.target=target
-       self.contextName=contextName
-       self.conversationId=conversationId
-   def run(self,query):
-    channel = grpc.insecure_channel(target=self.target)
-    stub = gpt_grpc.GptServiceStub(channel)
-    request = gpt.AskRequest(contextName=self.contextName, conversationId=self.conversationId, query=query)
-    answer = stub.ask(request)
-    response = answer.answer
-    channel.close()
-    return response 
+    def __init__(self, contextName, conversationId, target) -> None:
+        self.target = target
+        self.contextName = contextName
+        self.conversationId = conversationId
+
+    def run(self, query):
+        if not isinstance(query, str):
+            query = str(query)
+
+        channel = grpc.insecure_channel(target=self.target)
+        stub = gpt_grpc.GptServiceStub(channel)
+        request = gpt.AskRequest(contextName=self.contextName, conversationId=self.conversationId, query=query)
+        answer = stub.ask(request)
+        response = answer.answer
+        channel.close()
+        return response
