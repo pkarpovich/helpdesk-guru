e9ccbbf508d6f9c4b22b24a352302d99e53cd21d
Pavel Karpovich <pavel.karpovich@outlook.com>
2023-07-30 16:36:58 +0200

    feat(Dockerfile): update Dockerfile to use golang base image and install protobuf-compiler feat(Makefile): add new Makefile to build protobuf files feat(config.go): add APP_ENV environment variable to load .env file only in non-production environments refactor(Dockerfile, config.go): change working directory from /src to /app for better naming convention refactor(Dockerfile): update COPY commands to reflect new working directory and Makefile location refactor(config.go): extract .env loading logic to a separate function for better code organization

--- services/telegram-bot/config.go
+++ services/telegram-bot/config.go
@@ -4,26 +4,38 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 	"log"
+	"os"
 )
 
 type AppConfig struct {
 	GptServiceAddress string `envconfig:"GPT_SERVICE_ADDRESS" default:"localhost:50051"`
 	TelegramBotToken  string `envconfig:"TELEGRAM_BOT_TOKEN" required:"true"`
+	AppEnvironment    string `envconfig:"APP_ENV" default:"development"`
 	ContextName       string `envconfig:"CONTEXT_NAME" required:"true"`
 }
 
 func loadConfig() AppConfig {
 	var appConfig AppConfig
 
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	loadEnvVariablesIfNeeded()
 
-	err = envconfig.Process("", &appConfig)
+	err := envconfig.Process("", &appConfig)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
 	return appConfig
+}
+
+func loadEnvVariablesIfNeeded() {
+	env := os.Getenv("APP_ENV")
+	if env == "production" {
+		return
+	}
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 }
