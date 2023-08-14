b2d8e9efcf207df242a1c1f97896511ba6062bc7
MaximPyanin <maximpyanin@icloud.com>
2023-08-14 10:11:13 +0300

    Update client.py

--- services/instagram_service/main.py
+++ services/instagram_service/main.py
@@ -1,10 +1,10 @@
 import os
 import asyncio
 from dotenv import load_dotenv
-from services.instagram_service.src import client_grpc
+#from services.instagram_service.src import client_grpc
 from services.instagram_service.src import InstagramService
 from services.instagram_service.controller import instagram_Contoller
-
+from services.instagram_service.src import client_grpc
 
 load_dotenv()
 
