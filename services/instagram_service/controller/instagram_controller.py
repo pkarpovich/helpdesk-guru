10a815ac7986ea7bce18f713593db974455995e6
Maxim Pyanin <maximpyanin@icloud.com>
2023-08-21 10:53:03 +0300

    Update instagram_controller.py

--- services/instagram_service/controller/instagram_controller.py
+++ services/instagram_service/controller/instagram_controller.py
@@ -1,7 +1,7 @@
 import asyncio
 from services.instagram_service.src import client_grpc
 from services.instagram_service.src import InstagramService
-class instagram_Contoller:
+class InstagramContoller:
     def __int__(self, instagram_service: InstagramService, client: client_grpc) -> None:
         self.client = client
         self.instagram_service = instagram_service
