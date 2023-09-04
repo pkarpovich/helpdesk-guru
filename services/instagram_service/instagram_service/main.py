8505d1780b22c63fd217e54c143c660de497bd83
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-04 21:11:52 +0300

    Update main.py

--- services/instagram_service/instagram_service/main.py
+++ services/instagram_service/instagram_service/main.py
@@ -2,18 +2,22 @@ import os
 import asyncio
 
 from dotenv import load_dotenv
-from instagram_service.configs import AppConfig
-from instagram_service.controller import InstagramController
+
+from instagram_service.controller import InstagramContoller
 from instagram_service.services import InstagramService, GptService
+from instagram_service.services import AppConfig
 
 load_dotenv()
 
-config = AppConfig(os.environ)
-print(config)
-instagram_service=InstagramService(config=config)
-client=GptService(config=config)
-instagram_controller=InstagramContoller()
-instagram_controller.__int__(instagram_service, client)
-
+async def main():
+ config = AppConfig(os.environ)
+ instagram_service=InstagramService(config=config)
+ client=GptService(config=config)
+ instagram_controller=InstagramContoller()
+ instagram_controller.__int__(instagram_service, client)
+ await instagram_controller.start()
+ 
 if __name__=='__main__':
- asyncio.run(instagram_controller.start())
+ loop=asyncio.get_event_loop()
+ loop.run_until_complete(main())
+
