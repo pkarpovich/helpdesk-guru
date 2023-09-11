f0c5afe8e441891067f1620e5a6c440a98873df4
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-11 11:54:46 +0300

    Refactor

--- services/instagram-service/instagram_service/main.py
+++ services/instagram-service/instagram_service/main.py
@@ -2,20 +2,26 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
-
 from instagram_service.controller import InstagramContoller
-from instagram_service.services import AppConfig
-from instagram_service.services import InstagramService, GptService
+
+from instagram_service.services import (
+    AppConfig,
+    InstagramService,
+    GptService,
+)
 
 load_dotenv()
 
 
 async def main():
     config = AppConfig(os.environ)
+
     instagram_service = InstagramService(config=config)
     client = GptService(config=config)
+    
     instagram_controller = InstagramContoller()
     instagram_controller.__int__(instagram_service, client)
+    
     await instagram_controller.start()
 
 
