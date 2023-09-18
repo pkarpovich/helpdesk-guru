994f0e6bbe3f0132e6d69cc9f3298a9a715a644f
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-18 23:44:42 +0300

    Update test_instagram_service_unit.py

--- services/instagram-service/tests/test_instagram_service_unit.py
+++ services/instagram-service/tests/test_instagram_service_unit.py
@@ -10,28 +10,27 @@ from instagram_service.services import (
 @pytest.fixture
 def fix_ture():
     config=AppConfig(os.environ)
-    x=InstagramService(config)
+    instagram_service=InstagramService(config)
 
-    return x
+    return instagram_service
 
 def test_login_read(fix_ture,mocker):
     mocker.patch.object(fix_ture, 'login', return_value=None)
-    assert fix_ture.login() is None
+    assert fix_ture.login() == None
 
     mocker.patch.object(fix_ture, 'read_direct_messages', return_value=list)
-    assert fix_ture.read_direct_messages() is list
+    assert fix_ture.read_direct_messages() == list
 
 def test_login_send(fix_ture,mocker):
     mocker.patch.object(fix_ture, 'login', return_value=None)
-    assert fix_ture.login() is None
+    assert fix_ture.login() == None
 
     mocker.patch.object(fix_ture,'send_direct_message',return_value=None)
 
     assert isinstance(os.environ.get('TEST_MESSAGE'),str)
     assert isinstance(os.environ.get('TEST_SENDER_USERNAME'),str)
-    assert fix_ture.send_direct_message(os.environ.get('TEST_MESSAGE'), os.environ.get('TEST_SENDER_USERNAME')) is None
+    assert fix_ture.send_direct_message(os.environ.get('TEST_MESSAGE'), os.environ.get('TEST_SENDER_USERNAME')) == None
 
 def test_login(mocker, fix_ture):
     mocker.patch.object(fix_ture, 'login', return_value=None)
-    assert fix_ture.login() is None
-  
+    assert fix_ture.login() == None
