5b087676d5924c35973ac63ac200af87541a7394
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-18 23:25:31 +0300

    Create test_instagram_service_unit.py

---
+++ services/instagram-service/tests/test_instagram_service_unit.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+
+from instagram_service.services import (
+    AppConfig,
+    InstagramService,
+)
+
+
+@pytest.fixture
+def fix_ture():
+    config=AppConfig(os.environ)
+    x=InstagramService(config)
+
+    return x
+
+def test_login_read(fix_ture,mocker):
+    mocker.patch.object(fix_ture, 'login', return_value=None)
+    assert fix_ture.login() is None
+
+    mocker.patch.object(fix_ture, 'read_direct_messages', return_value=list)
+    assert fix_ture.read_direct_messages() is list
+
+def test_login_send(fix_ture,mocker):
+    mocker.patch.object(fix_ture, 'login', return_value=None)
+    assert fix_ture.login() is None
+
+    mocker.patch.object(fix_ture,'send_direct_message',return_value=None)
+
+    assert isinstance(os.environ.get('TEST_MESSAGE'),str)
+    assert isinstance(os.environ.get('TEST_SENDER_USERNAME'),str)
+    assert fix_ture.send_direct_message(os.environ.get('TEST_MESSAGE'), os.environ.get('TEST_SENDER_USERNAME')) is None
+
+def test_login(mocker, fix_ture):
+    mocker.patch.object(fix_ture, 'login', return_value=None)
+    assert fix_ture.login() is None
+  
