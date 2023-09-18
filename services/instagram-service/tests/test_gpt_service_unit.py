12e6cf87270bd6f7a205835cc493fa07c5ec0696
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-18 22:46:42 +0300

    Create test_gpt_service_unit.py

---
+++ services/instagram-service/tests/test_gpt_service_unit.py
@@ -0,0 +1,23 @@
+import os
+import pytest
+
+from instagram_service.services import (
+    GptService,
+    AppConfig,
+)
+
+
+@pytest.fixture
+def gpt_fixture():
+    config = AppConfig(os.environ)
+    gpt_service = GptService(config)
+    return gpt_service
+
+@pytest.mark.asyncio
+async def test_ask(gpt_fixture,mocker):
+    mocker.patch.object(gpt_fixture,'ask',return_value=str)
+    assert await gpt_fixture.ask(os.environ.get('TEST_QUERY')) == str
+
+
+if __name__=='__main__':
+    pytest.main()
