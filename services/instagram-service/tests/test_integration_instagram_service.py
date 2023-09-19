12e9b8a6406430aa951ffbc7ae4c73cf2175f0c5
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-19 21:36:12 +0200

    Update test_integration_instagram_service.py

--- services/instagram-service/tests/test_integration_instagram_service.py
+++ services/instagram-service/tests/test_integration_instagram_service.py
@@ -10,6 +10,7 @@ from instagram_service.services import (
 @pytest.fixture
 def fix_ture():
     config=AppConfig(os.environ)
+   
     instagram_service=InstagramService(config)
     instagram_service.login()
 
@@ -17,6 +18,7 @@ def fix_ture():
 
 def test_read(fix_ture):
    assert fix_ture.read_direct_messages()
+   
    messages = fix_ture.read_direct_messages()
    assert isinstance(messages, list)
 
@@ -24,5 +26,6 @@ def test_send(fix_ture):
     assert isinstance(os.environ.get('TEST_MESSAGE'),str)
     assert isinstance(os.environ.get('TEST_SENDER_USERNAME'),str)
     assert fix_ture.send_direct_message(os.environ.get('TEST_MESSAGE'), os.environ.get('TEST_SENDER_USERNAME'))
+   
     result = fix_ture.send_direct_message(os.environ.get('TEST_MESSAGE'), os.environ.get('TEST_SENDER_USERNAME'))
     assert result is None
