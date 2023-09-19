1cd0ad9f104b31da6ced5bd17cffa8bbd1b65303
Maxim Pyanin <maximpyanin@icloud.com>
2023-09-19 21:34:09 +0200

    Update test_integration_gpt_service.py

--- services/instagram-service/tests/test_integration_gpt_service.py
+++ services/instagram-service/tests/test_integration_gpt_service.py
@@ -1,13 +1,16 @@
 import os
+
 import pytest
 
 from instagram_service.services import GptService
 from instagram_service.services import AppConfig
 
+
 @pytest.fixture
 def gpt_fixture():
     config = AppConfig(os.environ)
     gpt_service = GptService(config)
+    
     return gpt_service
 
 @pytest.mark.asyncio
